6b4ec6be39ddd575938a931878c575a2746840b5
João Henrique de Oliveira Júnior <joaoho@itflex.com.br>
2016-02-19 10:10:10 -0200

    Correção de testes

--- Python/TDD_PYTHON/test_numerosTelefonicos.py
+++ Python/TDD_PYTHON/test_numerosTelefonicos.py
@@ -15,46 +15,46 @@ class TestNumerosTelefonicos(unittest.TestCase):
     def test_PrimeiraValidacao_Correta(self):
         # Devem haver no máximo 10 Dígitos, considerando DDD + Tipagem Fixo
         # + Número. ex: 55 xx [2-5] xxxxxxx
-        self.assertEqual(True, cl.PrimeiraValidacao('5511709091'))
+        self.assertTrue(cl.PrimeiraValidacao('5511709091'))
 
     def test_PrimeiraValidacao_Incorreta(self):
         # mais curto
-        self.assertEqual(False, cl.PrimeiraValidacao('123'))
+        self.assertFalse(cl.PrimeiraValidacao('123'))
         # maior que
-        self.assertEqual(False, cl.PrimeiraValidacao('1234567890111213'))
+        self.assertFalse(cl.PrimeiraValidacao('1234567890111213'))
         # sintax inválida
-        self.assertEqual(False, cl.PrimeiraValidacao('dasd321FDSF21as'))
+        self.assertFalse(cl.PrimeiraValidacao('dasd321FDSF21as'))
 
     def test_ValidacaoDDD_Correto(self):
         # Valem quaisquer dois dígitos
-        self.assertEqual(True, cl.ValidacaoDDD('55'))
-        self.assertEqual(True, cl.ValidacaoDDD('47'))
-        self.assertEqual(True, cl.ValidacaoDDD('11'))
+        self.assertTrue(cl.ValidacaoDDD('55'))
+        self.assertTrue(cl.ValidacaoDDD('47'))
+        self.assertTrue(cl.ValidacaoDDD('11'))
 
     def test_ValidacaoDDD_Incorreto(self):
         # Mais curto que
-        self.assertEqual(False, cl.ValidacaoDDD('1'))
+        self.assertFalse(cl.ValidacaoDDD('1'))
         # Começando com 0
-        self.assertEqual(False, cl.ValidacaoDDD('07'))
+        self.assertFalse(cl.ValidacaoDDD('07'))
         # Maior que
-        self.assertEqual(False, cl.ValidacaoDDD('112'))
+        self.assertFalse(cl.ValidacaoDDD('112'))
         # Não são números
-        self.assertEqual(False, cl.ValidacaoDDD('1E'))
+        self.assertFalse(cl.ValidacaoDDD('1E'))
 
     def test_validacaoFixo(self):
         # Verifica se está na faixa [2-5]
-        self.assertEqual(True, cl.ValidacaoFixo('23995530'))
-        self.assertEqual(True, cl.ValidacaoFixo('34496567'))
-        self.assertEqual(True, cl.ValidacaoFixo('44448774'))
-        self.assertEqual(True, cl.ValidacaoFixo('54537777'))
+        self.assertTrue(cl.ValidacaoFixo('23995530'))
+        self.assertTrue(cl.ValidacaoFixo('34496567'))
+        self.assertTrue(cl.ValidacaoFixo('44448774'))
+        self.assertTrue(cl.ValidacaoFixo('54537777'))
 
     def test_validacaoNaoFixo(self):
         # Verifica se não está na faixa [2-5]
-        self.assertEqual(False, cl.ValidacaoFixo('13995530'))
-        self.assertEqual(False, cl.ValidacaoFixo('64496567'))
-        self.assertEqual(False, cl.ValidacaoFixo('74448774'))
-        self.assertEqual(False, cl.ValidacaoFixo('84537777'))
-        self.assertEqual(False, cl.ValidacaoFixo('84537777'))
+        self.assertFalse(cl.ValidacaoFixo('13995530'))
+        self.assertFalse(cl.ValidacaoFixo('64496567'))
+        self.assertFalse(cl.ValidacaoFixo('74448774'))
+        self.assertFalse(cl.ValidacaoFixo('84537777'))
+        self.assertFalse(cl.ValidacaoFixo('84537777'))
 
 
 if __name__ == '__main__':
