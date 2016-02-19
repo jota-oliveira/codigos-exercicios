e3e4032dbc0d716085cdd757307759a871317a2c
João Henrique de Oliveira Júnior <joaoho@itflex.com.br>
2016-02-19 10:03:43 -0200

    Validação dos testes

--- Python/TDD_PYTHON/numerosTelefonicos.py
+++ Python/TDD_PYTHON/numerosTelefonicos.py
@@ -1,21 +1,29 @@
 # coding: utf-8
 
 
-class ValidaTelefones_Fixos():
+class ValidaTelefonesFixos():
 
-    # DDI = int
-    # DDD = int
-    # NUM = int
+    NUM = str
 
-    # def __init__(self, DDI, DDD, NUM):
-        # self.DDI = DDI
-        # self.DDD = DDD
-        # self.NUM = NUM
+    def __init__(self):
+        pass
 
-    def validacaoDDI(self, DDI):
-        if DDI == 55:
-            return 'Brasil'
-        elif DDI == 20:
-            return 'Egito'
-        elif DDI == 33:
-            return 'França'
+    def PrimeiraValidacao(self, COMPLETO):
+        if len(COMPLETO) == 10 and COMPLETO.isdigit():
+            return True
+        else:
+            return False
+
+    def ValidacaoDDD(self, DDD):
+        if len(DDD) == 2 and DDD.isdigit() and DDD[0] != "0":
+            return True
+        else:
+            return False
+
+    def ValidacaoFixo(self, NUM):
+        if len(NUM) == 8 and NUM.isdigit() and NUM[0] in ['2', '3', '4', '5']:
+            return True
+        else:
+            return False
+
+cl = ValidaTelefonesFixos()
--- Python/TDD_PYTHON/test_numerosTelefonicos.py
+++ Python/TDD_PYTHON/test_numerosTelefonicos.py
@@ -15,46 +15,46 @@ class TestNumerosTelefonicos(unittest.TestCase):
     def test_PrimeiraValidacao_Correta(self):
         # Devem haver no máximo 10 Dígitos, considerando DDD + Tipagem Fixo
         # + Número. ex: 55 xx [2-5] xxxxxxx
-        self.assertEqual(True, cl.PrimeiraValidacao(551170909))
+        self.assertEqual(True, cl.PrimeiraValidacao('5511709091'))
 
     def test_PrimeiraValidacao_Incorreta(self):
         # mais curto
-        self.assertEqual(False, cl.PrimeiraValidacao(123))
+        self.assertEqual(False, cl.PrimeiraValidacao('123'))
         # maior que
-        self.assertEqual(False, cl.PrimeiraValidacao(1234567890111213))
+        self.assertEqual(False, cl.PrimeiraValidacao('1234567890111213'))
         # sintax inválida
         self.assertEqual(False, cl.PrimeiraValidacao('dasd321FDSF21as'))
 
     def test_ValidacaoDDD_Correto(self):
         # Valem quaisquer dois dígitos
-        self.assertEqual(True, cl.ValidacaoDDI(55))
-        self.assertEqual(True, cl.ValidacaoDDI(47))
-        self.assertEqual(True, cl.ValidacaoDDI(11))
+        self.assertEqual(True, cl.ValidacaoDDD('55'))
+        self.assertEqual(True, cl.ValidacaoDDD('47'))
+        self.assertEqual(True, cl.ValidacaoDDD('11'))
 
     def test_ValidacaoDDD_Incorreto(self):
         # Mais curto que
-        self.assertEqual(True, cl.ValidacaoDDI(1))
+        self.assertEqual(False, cl.ValidacaoDDD('1'))
         # Começando com 0
-        self.assertEqual(True, cl.ValidacaoDDI(07))
+        self.assertEqual(False, cl.ValidacaoDDD('07'))
         # Maior que
-        self.assertEqual(True, cl.ValidacaoDDI(112))
+        self.assertEqual(False, cl.ValidacaoDDD('112'))
         # Não são números
-        self.assertEqual(True, cl.ValidacaoDDI('11'))
+        self.assertEqual(False, cl.ValidacaoDDD('1E'))
 
     def test_validacaoFixo(self):
         # Verifica se está na faixa [2-5]
-        self.assertEqual(True, cl.ValidacaoDDI(23995530))
-        self.assertEqual(True, cl.ValidacaoDDI(34496567))
-        self.assertEqual(True, cl.ValidacaoDDI(44448774))
-        self.assertEqual(True, cl.ValidacaoDDI(54537777))
+        self.assertEqual(True, cl.ValidacaoFixo('23995530'))
+        self.assertEqual(True, cl.ValidacaoFixo('34496567'))
+        self.assertEqual(True, cl.ValidacaoFixo('44448774'))
+        self.assertEqual(True, cl.ValidacaoFixo('54537777'))
 
     def test_validacaoNaoFixo(self):
         # Verifica se não está na faixa [2-5]
-        self.assertEqual(True, cl.ValidacaoDDI(13995530))
-        self.assertEqual(True, cl.ValidacaoDDI(64496567))
-        self.assertEqual(True, cl.ValidacaoDDI(74448774))
-        self.assertEqual(True, cl.ValidacaoDDI(84537777))
-        self.assertEqual(True, cl.ValidacaoDDI('84537777'))
+        self.assertEqual(False, cl.ValidacaoFixo('13995530'))
+        self.assertEqual(False, cl.ValidacaoFixo('64496567'))
+        self.assertEqual(False, cl.ValidacaoFixo('74448774'))
+        self.assertEqual(False, cl.ValidacaoFixo('84537777'))
+        self.assertEqual(False, cl.ValidacaoFixo('84537777'))
 
 
 if __name__ == '__main__':
